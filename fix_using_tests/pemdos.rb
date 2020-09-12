a136154bbec14adc7569a6a4a6c69a4fd3d95a1d
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-12 23:12:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,11 +2,11 @@
 require 'pry'
 
 
-validates_numericality_of = 10.to_s
+number = 10.to_s
 def snake_it_up(string)
   if string[0] == "s"
-  letter * "s" + string)
-  else
+  number * "s" + string)
+else
     string
   end
 end
